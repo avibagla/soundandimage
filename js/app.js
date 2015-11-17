1a6092a516d637aa1fe06e7513b1cf9f02cbcd9b
Avi Bagla <avibagla@gmail.com>
2015-11-17 11:39:05 -0800

    woo starting sound and image proect

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+
+var player;
+
+$(document).ready(function(){
+
+});
+
+
+
+function onYouTubeIframeAPIReady(){
+	player
+}
